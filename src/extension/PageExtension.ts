5c8212e61416ad7b7f3f7302c2d03ce5825fc50c
Cassie <348040933@qq.com>
2023-05-25 08:29:02 +0000

    完善删除逻辑
    Signed-off-by: Cassie <348040933@qq.com>

--- src/extension/PageExtension.ts
+++ src/extension/PageExtension.ts
@@ -1,4 +1,4 @@
-import { Extension } from "@tiptap/core";
+import { Extension, findParentNode } from "@tiptap/core";
 import { EmrDocument } from "@/extension/doc/document";
 import { Page } from "@/extension/page/page";
 import CassieBlock from "@/extension/node/CassieBlock";
@@ -7,7 +7,8 @@ import { CassieBlockExt } from "@/extension/node/CassieBlockExt";
 import * as commands from "@/extension/commands/index";
 import { paginationPlugin } from "@/extension/page/pagePlugn";
 import Image from "@tiptap/extension-image";
-
+import { Selection, TextSelection } from "@tiptap/pm/state";
+import { PAGE } from "./nodeNames";
 export const PageExtension = Extension.create<PageOptions>({
   name: "PageExtension",
   /*添加自定义命令*/
@@ -61,8 +62,72 @@ export const PageExtension = Extension.create<PageOptions>({
       this.editor.commands.first(({ commands }) => {
         return [() => commands.newlineInCode(), () => commands.createParagraphNear(), () => commands.liftEmptyBlock(), () => commands.splitCBlock()];
       });
+    const handleBackspace = () =>
+      this.editor.commands.first(({ commands }) => [
+        () => {
+          const ok = commands.undoInputRule();
+          console.log(ok);
+          return ok;
+        },
+        // maybe convert first text block node to default node
+        () =>
+          commands.command(({ tr }) => {
+            const { selection, doc } = tr;
+            const { empty, $anchor } = selection;
+            const { pos, parent } = $anchor;
+            const isAtStart = Selection.atStart(doc).from === pos;
+            if (!empty || !isAtStart || !parent.type.isTextblock || parent.textContent.length) {
+              return false;
+            }
+            const ok = commands.clearNodes();
+            console.log(ok);
+            return ok;
+          }),
+        () => {
+          const ok = commands.deleteSelection();
+          console.log(ok);
+          return ok;
+        },
+        () => {
+          const ok = commands.joinBackward();
+          console.log(ok);
+          return ok;
+        },
+        () => {
+          const ok = commands.selectNodeBackward();
+          console.log(ok);
+          return ok;
+        },
+        () =>
+          commands.command(({ tr }) => {
+            const { selection, doc } = tr;
+            const { $anchor } = selection;
+            const { pos } = $anchor;
+            if (doc.childCount == 1) return false;
+            if (Selection.atEnd(doc).from === pos) {
+              return commands.deleteNode(PAGE);
+            }
+            const pageNode = findParentNode((node) => node.type.name === PAGE)(selection);
+            if (pageNode) {
+              const isAtStart = pageNode.start + Selection.atStart(pageNode.node).from === pos;
+              if (isAtStart) {
+                const vm = TextSelection.create(doc, pos - 20, pos - 20);
+                const beforePageNode = findParentNode((node) => node.type.name === PAGE)(vm);
+                if (beforePageNode) {
+                  const pos1 = Selection.atEnd(beforePageNode.node).from + beforePageNode.start;
+                  const after = TextSelection.create(doc, pos1, pos1);
+                  tr.setSelection(after);
+                  return true;
+                }
+              }
+            }
+
+            return false;
+          })
+      ]);
     return {
-      Enter: handleEnter
+      Enter: handleEnter,
+      Backspace: handleBackspace
     };
   }
 });
