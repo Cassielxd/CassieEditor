4aff34ec15da1b80ba7328e81c2dabaf6795e799
李兴栋 <lixingdong@aliyun.com>
2023-05-29 17:15:27 +0800

    添加打包优化

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,14 @@ export default defineConfig({
       }
     ]
   },
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 });
