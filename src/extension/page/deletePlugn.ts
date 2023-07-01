ee374cfd4a545f5606e299950eca71793f1a4423
李兴栋 <lixingdong@aliyun.com>
2023-07-01 11:59:52 +0800

    ...

--- src/extension/page/deletePlugn.ts
+++ src/extension/page/deletePlugn.ts
@@ -27,20 +27,19 @@ export const deletePlugin = (editor: Editor) => {
     },
     appendTransaction([newTr], _prevState, state) {
       const { specialdelete } = this.getState(state);
-      let { selection, tr, doc } = state;
-      const { $anchor } = selection;
+      let { tr } = state;
+      const { $anchor } = state.selection;
       const { pos } = $anchor;
       if (specialdelete) {
         /*
          * 获取上一页的 的 最后一点 将光标设置过去
          * */
-        const vm = TextSelection.create(doc, pos - 20, pos - 20);
+        const vm = TextSelection.create(state.doc, pos - 20, pos - 20);
         const beforePageNode = findParentNode((node) => node.type.name === PAGE)(vm);
         //找到上一个page 获取到最后一个点 然后设置 光标选中
         if (beforePageNode) {
           const pos1 = Selection.atEnd(beforePageNode.node).from + beforePageNode.start;
-          const after = TextSelection.create(doc, pos1, pos1);
-          tr = tr.setSelection(after);
+          tr.step(new ReplaceStep(pos1, pos, Slice.empty));
           return tr.scrollIntoView();
         }
       }
