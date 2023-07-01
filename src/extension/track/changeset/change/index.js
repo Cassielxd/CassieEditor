a8620484655df787fcec7c9e80ed98c74ee06584
李兴栋 <lixingdong@aliyun.com>
2023-07-01 09:14:13 +0800

    ...

--- src/extension/track/changeset/change/index.js
+++
@@ -1,5 +0,0 @@
-export { BlockChange } from "./block-change";
-export { Change } from "./change";
-export { Span } from "./span";
-export { merge } from "./merge";
-//# sourceMappingURL=index.js.map
