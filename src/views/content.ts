bdde3b66bc9dd2010054cc2c04c23fa9ef807bc6
李兴栋 <lixingdong@aliyun.com>
2023-07-04 09:02:19 +0800

    ...

--- src/views/content.ts
+++ src/views/content.ts
@@ -222,8 +222,33 @@ export const newContent = {
       attrs: {
         HTMLAttributes: null,
         pageNumber: 1,
+        id: "6759fec0-05b7-4a5b-ae04-16bfb6fba9bc"
       },
       content: [
+        {
+          type: "Node",
+          attrs: {
+            id: "pid_352165982dd",
+            title: null,
+            group: "title"
+          },
+          content: [
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "0b4e04a4-51ea-47e7-ba20-a5042663c729",
+                group: "p1"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "记录时间： 日期"
+                }
+              ]
+            }
+          ]
+        },
         {
           type: "Node",
           attrs: {
@@ -236,12 +261,135 @@ export const newContent = {
               type: "paragraph",
               attrs: {
                 textAlign: "left",
+                id: "fea620f0-dd3c-4ab5-8c85-752b51996a98",
                 group: "p2"
               },
               content: [
                 {
                   type: "text",
-                  text: "这是一测试文啵啵啵啵啵啵" //"这是一个测试文"
+                  text: "气管哮喘病史否认支,按国家计划接种"
+                },
+                {
+                  type: "text",
+                  text: "卡介苗、乙肝、脊髓灰质炎、百白破、麻疹及乙脑疫苗."
+                }
+              ]
+            }
+          ]
+        },
+        {
+          type: "Node",
+          attrs: {
+            id: "pid_3521659828",
+            title: "诊断结果",
+            group: "body_result"
+          },
+          content: [
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "bb1a5ec3-88ca-4a64-b69c-4a0361f5e434",
+                group: "p3"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "诊断结果正常 ,我是过渡语句 测试 日期 测试2"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          type: "Node",
+          attrs: {
+            id: "pid_3808237023",
+            title: "体格检查",
+            group: "body_check"
+          },
+          content: [
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "c5ae0525-bcaf-4117-8208-804f46612574",
+                group: "p4"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "发育正常，营养良好，神志清楚，主动体位，查体合作,步态正常，正常面容。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "c576357a-1eeb-49f3-8d12-ec214049ed83",
+                group: "p5"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "全身皮肤黏膜无黄染、无水肿，未见出血点、未见蜘蛛痣， 未见瘘管。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "04f2c624-b004-46ac-a5c0-4d3b02abc2e3",
+                group: "p6"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "头颅形态大小无畸形，无肿块。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "bed29b51-a49d-4ed4-866d-d35a42b472d8",
+                group: "p7"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "颜面及眼睑无浮肿，双侧瞳孔等大同圆直径约3mm，对光反应灵敏。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "20acbf06-1e2d-47c0-943b-262d5b33b3b0",
+                group: "p8"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "口腔无异味，口唇红润光泽，黏膜正常，伸舌居中,无震颤。 耳鼻喉科情况详见专科检查。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "04d77859-c088-4c4f-af8b-17500c235e97",
+                group: "p9"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "胸廓双侧对称正常，胸壁无压痛。呼吸运动自如，语音震颤双侧对称，胸壁无压痛。"
                 }
               ]
             }
@@ -260,8 +408,33 @@ export const pageOldContent = {
       attrs: {
         HTMLAttributes: null,
         pageNumber: 1,
+        id: "6759fec0-05b7-4a5b-ae04-16bfb6fba9bc"
       },
       content: [
+        {
+          type: "Node",
+          attrs: {
+            id: "pid_352165982dd",
+            title: null,
+            group: "title"
+          },
+          content: [
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "0b4e04a4-51ea-47e7-ba20-a5042663c729",
+                group: "p1"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "记录时间： 日期"
+                }
+              ]
+            }
+          ]
+        },
         {
           type: "Node",
           attrs: {
@@ -274,18 +447,140 @@ export const pageOldContent = {
               type: "paragraph",
               attrs: {
                 textAlign: "left",
+                id: "fea620f0-dd3c-4ab5-8c85-752b51996a98",
                 group: "p2"
               },
               content: [
                 {
                   type: "text",
-                  text: "这是一个测试文啊啊啊啊啊啊"
+                  text: "气管哮喘病史否认支,按国家计划接种"
+                },
+                {
+                  type: "text",
+                  text: "卡介苗、乙肝、脊髓灰质炎."
+                }
+              ]
+            }
+          ]
+        },
+        {
+          type: "Node",
+          attrs: {
+            id: "pid_3521659828",
+            title: "诊断结果",
+            group: "body_result"
+          },
+          content: [
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "bb1a5ec3-88ca-4a64-b69c-4a0361f5e434",
+                group: "p3"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "诊断结果正常 ,我是过渡语句 测试 日期 测试2"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          type: "Node",
+          attrs: {
+            id: "pid_3808237023",
+            title: "体格检查",
+            group: "body_check"
+          },
+          content: [
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "c5ae0525-bcaf-4117-8208-804f46612574",
+                group: "p4"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "发育正常，营养良好，神志清楚，主动体位，查体合作,步态正常，正常面容。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "c576357a-1eeb-49f3-8d12-ec214049ed83",
+                group: "p5"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "全身皮肤黏膜无黄染、无水肿，未见出血点、未见蜘蛛痣，全身浅表淋巴结未扪及肿大， 未见瘘管。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "04f2c624-b004-46ac-a5c0-4d3b02abc2e3",
+                group: "p6"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "头颅形态大小无畸形，畸形颅,无压痛,无肿块。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "bed29b51-a49d-4ed4-866d-d35a42b472d8",
+                group: "p7"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "颜面及眼睑无浮肿，双侧瞳孔等大同圆直径约3mm，对光反应灵敏。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "20acbf06-1e2d-47c0-943b-262d5b33b3b0",
+                group: "p8"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "口腔无异味，口唇红润光泽，黏膜正常，伸舌居中,无震颤。 耳鼻喉科情况详见专科检查。"
+                }
+              ]
+            },
+            {
+              type: "paragraph",
+              attrs: {
+                textAlign: "left",
+                id: "04d77859-c088-4c4f-af8b-17500c235e97",
+                group: "p9"
+              },
+              content: [
+                {
+                  type: "text",
+                  text: "胸廓双侧对称正常，呼吸运动自如，语音震颤双侧对称，胸壁无压痛。"
                 }
               ]
             }
           ]
         }
-
       ]
     }
   ]
