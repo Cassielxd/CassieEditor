f27d89ec4b42165194651b57980a1ab59f90e6db
李兴栋 <lixingdong@aliyun.com>
2023-07-18 13:48:08 +0800

    ....

--- src/components/CassieDate.vue
+++ src/components/CassieDate.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="card bg-base-100 shadow-xl">
-    <v-calendar @dayclick="onClick" />
-    <div class="card-actions justify-end">
-      <button class="btn btn-accent btn-xs" @click="onSubmit">确定</button>
-      <button class="btn btn-ghost btn-xs" @click="quit">取消</button>
+    <figure><v-calendar @dayclick="onClick" /></figure>
+    <div class="card-actions justify-end px-4 py-2">
+      <div class="badge badge-outline" @click="onSubmit">确定</div>
+      <div class="badge badge-outline" @click="quit">取消</div>
     </div>
   </div>
 </template>
